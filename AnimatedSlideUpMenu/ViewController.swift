1ca61bdbff16d636070fa7e1e58715aadd2a80e1
Jianyuan Chen <jianyuan.chen12@gmail.com>
2021-04-27 20:19:59 -0700

    Initial Commit

---
+++ AnimatedSlideUpMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedSlideUpMenu
+//
+//  Created by Jianyuan Chen on 2021-04-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
